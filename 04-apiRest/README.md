9ec1f8bdbeb9efbdeae5113afd71c96c283e6cf3
Massaaki <mauricio.massaaki@gmail.com>
2021-03-21 18:17:39 -0300

    feat(apiRest/database): Add Sequelize and pg && add first migration

---
+++ 04-apiRest/README.md
@@ -0,0 +1,5 @@
+## Create migration
+
+```migration
+npx sequelize migration:create --name=SOME_MIGRATION_NAME
+```
---
+++ 04-apiRest/src/database/migrations/20210321210424-students.js
@@ -0,0 +1,40 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('students', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      firstName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      age: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('students');
+  },
+};
