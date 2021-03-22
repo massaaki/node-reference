69e4f603bc304d3faf61f6f8ff6cec9f2b71e481
Massaaki <mauricio.massaaki@gmail.com>
2021-03-22 13:32:05 -0300

    feat(apiRest/student): Add crud

--- 04-apiRest/src/controllers/StudentController.js
+++ 04-apiRest/src/controllers/StudentController.js
@@ -5,6 +5,75 @@ class StudentController {
     const response = await Student.findAll();
     res.json(response);
   }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({ errors: ['id not found'] });
+      }
+      const response = await Student.findByPk(id);
+
+      if (!response) {
+        return res.status(400).json({ errors: ['student not found'] });
+      }
+
+      return res.json(response);
+    } catch (e) {
+      return res.status(400).json({ errirs: e.errors.map((err) => err.message) });
+    }
+  }
+
+  async create(req, res) {
+    try {
+      const student = await Student.create(req.body);
+
+      return res.json(student);
+    } catch (e) {
+      return res.status(400).json({ errors: e.errors.map((err) => err.message) });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({ errors: ['id not found'] });
+      }
+      const response = await Student.findByPk(id);
+
+      if (!response) {
+        return res.status(400).json({ errors: ['student not found'] });
+      }
+
+      const newStudent = await response.update(req.body);
+
+      return res.json(newStudent);
+    } catch (e) {
+      return res.status(400).json({ errors: e.errors.map((err) => err.message) });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({ errors: ['id not found'] });
+      }
+      const response = await Student.findByPk(id);
+
+      if (!response) {
+        return res.status(400).json({ errors: ['student not found'] });
+      }
+      await response.destroy();
+      return res.json(response);
+    } catch (e) {
+      return res.status(400).json({ errors: e.errors.map((err) => err.message) });
+    }
+  }
 }
 
 export default new StudentController();
--- 04-apiRest/src/models/Student.js
+++ 04-apiRest/src/models/Student.js
@@ -3,10 +3,44 @@ import Sequelize, { Model } from 'sequelize';
 export default class Student extends Model {
   static init(sequelize) {
     super.init({
-      email: Sequelize.STRING,
-      firstName: Sequelize.STRING,
-      lastName: Sequelize.STRING,
-      age: Sequelize.INTEGER,
+      email: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          isEmail: {
+            msg: 'Invalid email',
+          },
+        },
+      },
+      firstName: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 255],
+            msg: 'Field error..: [min:3 | max:255]',
+          },
+        },
+      },
+      lastName: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 255],
+            msg: 'Field error..: [min:3 | max:255]',
+          },
+        },
+      },
+      age: {
+        type: Sequelize.INTEGER,
+        defaultValue: '',
+        validate: {
+          isInt: {
+            msg: 'type must be a number',
+          },
+        },
+      },
     }, {
       sequelize,
     });
